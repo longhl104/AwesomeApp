de414e2259f5a861abc3a8445fc374e3a9a8165d
Long Nguyen Duy <dngu0797@uni.sydney.edu.au>
2020-01-08 22:44:56 +1100

    Add project files.

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You tapped {count} times.";
+            if(count >= 1000)
+            {
+                text.Text = "\"People say nothing is impossible,\nbut I do nothing every day.\" - A. A. Milne";
+            }
+        }
+    }
+}
